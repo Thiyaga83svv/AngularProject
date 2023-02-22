d3f351851cff488e816506d2f56ed21d4ffa965f
thiyagarajan ramu <thiyagu83svv@gmail.com>
2023-02-23 09:36:24 +1100

    First Angular project

---
+++ apm-new/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: 
+  `<div> <h1>{{pagetitle}}</h1>
+  <pm-products></pm-products>
+  </div>`
+})
+export class AppComponent {
+  pagetitle = 'Product Management';
+}
